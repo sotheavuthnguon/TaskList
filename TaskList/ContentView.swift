0d1da671f0aa1db830045dcc0dd6f70e4a72092e
Sotheavuth Nguon <sotheavuth.nguon@yahoo.com>
2022-06-18 10:56:53 +0700

    Initial Commit

---
+++ TaskList/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TaskList
+//
+//  Created by Sotheavuth Nguon on 18/6/22.
+//  Copyright (c) 2022 Canadia Bank Plc. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskList/TaskListApp.swift
@@ -0,0 +1,19 @@
+//
+//  TaskListApp.swift
+//  TaskList
+//
+//  Created by Sotheavuth Nguon on 18/6/22.
+//  Copyright (c) 2022 Canadia Bank Plc. All rights reserved.
+//
+
+
+import SwiftUI
+
+@main
+struct TaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
