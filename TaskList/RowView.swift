a8cfdd8b6c82d0acf8f10c53889caa5f30d677fb
Sotheavuth Nguon <sotheavuth.nguon@yahoo.com>
2022-06-18 12:42:58 +0700

    Task completion

--- TaskList/RowView.swift
+++ TaskList/RowView.swift
@@ -11,9 +11,16 @@ import SwiftUI
 
 struct RowView: View {
   @Binding var task: Task
+  let checkmark = Image(systemName: "checkmark")
   var body: some View {
     NavigationLink(destination: TaskEditingView(task: $task)) {
+      if task.completed {
+        checkmark
+      } else {
+        EmptyView()
+      }
       Text(task.name)
+        .strikethrough(task.completed)
     }
   }
 }
